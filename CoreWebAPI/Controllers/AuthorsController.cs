e1b9466a909d10f0fd71fb4cc11c48f83ffbec10
jigneshkmistry <jignesh@trivediat.com>
2019-01-21 18:35:51 +0530

    Add project files.

---
+++ CoreWebAPI/Controllers/AuthorsController.cs
@@ -0,0 +1,22 @@
+﻿using CoreWebAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebAPI.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        private ILibraryRepository _libraryRepository;
+
+        public AuthorsController(ILibraryRepository libraryRepository)
+        {
+            _libraryRepository = libraryRepository;
+        }
+
+        [HttpGet("api/authors")]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _libraryRepository.GetAuthors();
+            return new JsonResult(authorsFromRepo);
+        }
+    }
+}
---
+++ CoreWebAPI/Entities/LibraryContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoreWebAPI.Entities
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+}
